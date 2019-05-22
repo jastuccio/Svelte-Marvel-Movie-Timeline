52688bdc3e8043c4dfa61b9727b06ab4f4e8c072
Joe <joe@astucc.io>
2019-05-22 17:30:06 -0400

    feat: Add Movies and Scroll Buttons

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,37 @@
 <script>
-	export let name;
+  import MarvelMovies from "./MarvelMovies.js";
+
+  let index = 0;
+
+  function nextMovie() {
+    index = (index + 1) % MarvelMovies.length;
+  }
+
+  function prevMovie() {
+    index = index - 1;
+    if (index < 0) {
+      index = MarvelMovies.length - 1;
+    }
+  }
 </script>
 
 <style>
-	h1 {
-		color: purple;
-	}
+  h1 {
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Marvel Move Timeline</h1>
+
+<button on:click={prevMovie}>👈</button>
+
+<p>Current movie: {MarvelMovies[index].title}</p>
+
+<button on:click={nextMovie}>👉</button>
+
+<p>
+  Borrowed from
+  <a
+    href="https://www.amctheatres.com/amc-scene/the-best-way-to-rewatch-the-mcu">
+    AMC Movies | Watch The Marvel Movies In Order
+  </a>
+</p>
